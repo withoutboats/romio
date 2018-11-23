f7691b18e948ec518c1873f27d7efb954ab49f25
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-11-23 11:23:51 +0100

    allow docs to build
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,12 @@
 //! Or if thou wilt not, be but sworn my love
 //! And I'll no longer be asynchronous
 #![feature(futures_api, pin, arbitrary_self_types)]
-
 #![doc(html_root_url = "https://docs.rs/tokio-reactor/0.1.6")]
-#![deny(missing_docs, warnings, missing_debug_implementations)]
+#![deny(missing_docs, missing_debug_implementations)]
+#![cfg_attr(test, deny(warnings))]
 
 pub mod net;
 pub mod reactor;
 
 #[cfg(unix)]
 pub mod uds;
-
