b085e6c10288295f7bc1614d5456690d1205ca4f
Without Boats <boats@mozilla.com>
2018-11-25 16:02:17 +0100

    Reduce public API of net module

--- src/net/tcp/stream.rs
+++ src/net/tcp/stream.rs
@@ -1,7 +1,7 @@
 use std::fmt;
 use std::io;
 use std::mem;
-use std::net::{self, SocketAddr, Shutdown};
+use std::net::{SocketAddr, Shutdown};
 use std::pin::Pin;
 use std::time::Duration;
 
@@ -11,7 +11,7 @@ use futures::task::LocalWaker;
 use iovec::IoVec;
 use mio;
 
-use crate::reactor::{Handle, PollEvented};
+use crate::reactor::PollEvented;
 
 /// An I/O object representing a TCP stream connected to a remote endpoint.
 ///
@@ -62,56 +62,6 @@ impl TcpStream {
     pub(crate) fn new(connected: mio::net::TcpStream) -> TcpStream {
         let io = PollEvented::new(connected);
         TcpStream { io }
-    }
-
-    /// Create a new `TcpStream` from a `net::TcpStream`.
-    ///
-    /// This function will convert a TCP stream created by the standard library
-    /// to a TCP stream ready to be used with the provided event loop handle.
-    /// Use `Handle::default()` to lazily bind to an event loop, just like `connect` does.
-    pub fn from_std(stream: net::TcpStream, handle: &Handle)
-        -> io::Result<TcpStream>
-    {
-        let io = mio::net::TcpStream::from_stream(stream)?;
-        let io = PollEvented::new_with_handle(io, handle)?;
-
-        Ok(TcpStream { io })
-    }
-
-    /// Creates a new `TcpStream` from the pending socket inside the given
-    /// `std::net::TcpStream`, connecting it to the address specified.
-    ///
-    /// This constructor allows configuring the socket before it's actually
-    /// connected, and this function will transfer ownership to the returned
-    /// `TcpStream` if successful. An unconnected `TcpStream` can be created
-    /// with the `net2::TcpBuilder` type (and also configured via that route).
-    ///
-    /// The platform specific behavior of this function looks like:
-    ///
-    /// * On Unix, the socket is placed into nonblocking mode and then a
-    ///   `connect` call is issued.
-    ///
-    /// * On Windows, the address is stored internally and the connect operation
-    ///   is issued when the returned `TcpStream` is registered with an event
-    ///   loop. Note that on Windows you must `bind` a socket before it can be
-    ///   connected, so if a custom `TcpBuilder` is used it should be bound
-    ///   (perhaps to `INADDR_ANY`) before this method is called.
-    pub fn connect_std(stream: net::TcpStream,
-                       addr: &SocketAddr,
-                       handle: &Handle)
-        -> ConnectFuture
-    {
-        use self::ConnectFutureState::*;
-
-        let io = mio::net::TcpStream::connect_stream(stream, addr)
-            .and_then(|io| PollEvented::new_with_handle(io, handle));
-
-        let inner = match io {
-            Ok(io) => Waiting(TcpStream { io }),
-            Err(e) => Error(e),
-        };
-
-        ConnectFuture { inner: inner }
     }
 
     /// Check the TCP stream's read readiness state.
@@ -328,17 +278,6 @@ impl TcpStream {
     pub fn set_linger(&self, dur: Option<Duration>) -> io::Result<()> {
         self.io.get_ref().set_linger(dur)
     }
-
-    /// Creates a new independently owned handle to the underlying socket.
-    ///
-    /// The returned `TcpStream` is a reference to the same stream that this
-    /// object references. Both handles will read and write the same stream of
-    /// data, and options set on one stream will be propagated to the other
-    /// stream.
-    pub fn try_clone(&self) -> io::Result<TcpStream> {
-        let io = self.io.get_ref().try_clone()?;
-        Ok(TcpStream::new(io))
-    }
 }
 
 // ===== impl Read / Write =====
@@ -497,20 +436,6 @@ mod sys {
             self.io.get_ref().as_raw_fd()
         }
     }
-}
-
-#[cfg(windows)]
-mod sys {
-    // TODO: let's land these upstream with mio and then we can add them here.
-    //
-    // use std::os::windows::prelude::*;
-    // use super::TcpStream;
-    //
-    // impl AsRawHandle for TcpStream {
-    //     fn as_raw_handle(&self) -> RawHandle {
-    //         self.io.get_ref().as_raw_handle()
-    //     }
-    // }
 }
 
 fn is_wouldblock<T>(r: &io::Result<T>) -> bool {
